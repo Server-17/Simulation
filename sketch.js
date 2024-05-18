2560f3ded66899312be0088126c954a905aff796
Jorin Eggers <jorineggers@gmail.com>
2024-05-18 09:48:52 +0200

    first commit

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(500, 500);
+    // schreibe hier Code, der einmal ausgeführt werden soll
+
+    // Beispiel: Ein Rechteck zeichnen
+    rect(10, 10, 50, 50);
+}
+
+function draw() {
+    // der Code hier wird automatisch dauerhaft wiederholt
+}
