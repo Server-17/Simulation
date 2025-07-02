f1adea548cdaabff92430d54995eb602ddf77dd6
Jorin Eggers <you@example.com>
2025-07-02 16:01:18 +0200

    update tests.js to match position.row and position.col method

--- tests.js
+++ tests.js
@@ -43,7 +43,7 @@ function drawMatrix(matrix) {
 function visualizeNeighbourPositions(neighbours) {
     fill("orange");
     for (let neighbour of neighbours) {
-        ellipse((neighbour[1] + 0.5) * blockSize, (neighbour[0] + 0.5) * blockSize, blockSize * 0.5, blockSize * 0.5);
+        ellipse((neighbour.col + 0.5) * blockSize, (neighbour.row + 0.5) * blockSize, blockSize * 0.5, blockSize * 0.5);
     }
 }
 
@@ -55,4 +55,4 @@ function visualizeSearchRange(row, col, distance) {
     rect((col - distance) * blockSize, (row - distance) * blockSize, blocks * blockSize, blocks * blockSize);
     rect(col * blockSize, row * blockSize, blockSize, blockSize);
     strokeWeight(1);
-}
+}  
